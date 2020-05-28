sobotka/blender-addons

commit 6aa8e130eff59059886e203ff95221609f63b222

Update for renaming lamp to light.

io_scene_x3d/import_x3d.py
@@ -2790,9 +2790,9 @@ def appearance_LoadTexture(tex_node, ancestry, node):
 
     if bpyima:  # Loading can still fail
         repeat_s = tex_node.getFieldAsBool('repeatS', True, ancestry)
-        bpyima.use_clamp_x = not repeat_s
+        bpyima.use_clight_x = not repeat_s
         repeat_t = tex_node.getFieldAsBool('repeatT', True, ancestry)
-        bpyima.use_clamp_y = not repeat_t
+        bpyima.use_clight_y = not repeat_t
 
         # Update the desc-based cache
         if desc:
@@ -3148,7 +3148,7 @@ def importLamp_PointLight(node, ancestry):
     # is_on = node.getFieldAsBool('on', True, ancestry) # TODO
     radius = node.getFieldAsFloat('radius', 100.0, ancestry)
 
-    bpylamp = bpy.data.lamps.new(vrmlname, 'POINT')
+    bpylamp = bpy.data.lights.new(vrmlname, 'POINT')
     bpylamp.energy = intensity
     bpylamp.distance = radius
     bpylamp.color = color
@@ -3169,7 +3169,7 @@ def importLamp_DirectionalLight(node, ancestry):
     intensity = node.getFieldAsFloat('intensity', 1.0, ancestry)  # max is documented to be 1.0 but some files have higher.
     # is_on = node.getFieldAsBool('on', True, ancestry) # TODO
 
-    bpylamp = bpy.data.lamps.new(vrmlname, 'SUN')
+    bpylamp = bpy.data.lights.new(vrmlname, 'SUN')
     bpylamp.energy = intensity
     bpylamp.color = color
 
@@ -3197,7 +3197,7 @@ def importLamp_SpotLight(node, ancestry):
     # is_on = node.getFieldAsBool('on', True, ancestry) # TODO
     radius = node.getFieldAsFloat('radius', 100.0, ancestry)
 
-    bpylamp = bpy.data.lamps.new(vrmlname, 'SPOT')
+    bpylamp = bpy.data.lights.new(vrmlname, 'SPOT')
     bpylamp.energy = intensity
     bpylamp.distance = radius
     bpylamp.color = color

add_advanced_objects_menu/__init__.py
@@ -108,8 +108,8 @@ class INFO_MT_scene_elements_add(Menu):
 
 
 # Define the "Lights" menu
-class INFO_MT_mesh_lamps_add(Menu):
-    bl_idname = "INFO_MT_scene_lamps"
+class INFO_MT_mesh_lights_add(Menu):
+    bl_idname = "INFO_MT_scene_lights"
     bl_label = "Lighting Sets"
 
     def draw(self, context):
@@ -191,7 +191,7 @@ def menu(self, context):
     layout.operator_context = 'INVOKE_REGION_WIN'
     self.layout.separator()
     self.layout.menu("INFO_MT_scene_elements", icon="SCENE_DATA")
-    self.layout.menu("INFO_MT_scene_lamps", icon="LAMP_SPOT")
+    self.layout.menu("INFO_MT_scene_lights", icon="LIGHT_SPOT")
     self.layout.separator()
     self.layout.menu("INFO_MT_array_mods", icon="MOD_ARRAY")
     self.layout.menu("INFO_MT_quick_tools", icon="MOD_BUILD")

add_advanced_objects_menu/add_light_template.py
@@ -5,10 +5,10 @@ from bpy.types import Operator
 from bpy.props import BoolProperty
 
 
-def add_lamps(self, context):
+def add_lights(self, context):
 
     if self.bKeyLight:
-        keyLight = bpy.data.lamps.new(name="Key_Light", type="SPOT")
+        keyLight = bpy.data.lights.new(name="Key_Light", type="SPOT")
         ob = bpy.data.objects.new("Key_Light", keyLight)
         constraint = ob.constraints.new(type='COPY_LOCATION')
         constraint.use_offset = True
@@ -23,7 +23,7 @@ def add_lamps(self, context):
         ob.rotation_euler[2] = -0.785398
 
     if self.bFillLight:
-        fillLight = bpy.data.lamps.new(name="Fill_Light", type="SPOT")
+        fillLight = bpy.data.lights.new(name="Fill_Light", type="SPOT")
         ob = bpy.data.objects.new("Fill_Light", fillLight)
         constraint = ob.constraints.new(type='COPY_LOCATION')
         constraint.use_offset = True
@@ -39,7 +39,7 @@ def add_lamps(self, context):
         ob.data.energy = 0.3
 
     if self.bBackLight:
-        backLight = bpy.data.lamps.new(name="Back_Light", type="SPOT")
+        backLight = bpy.data.lights.new(name="Back_Light", type="SPOT")
         ob = bpy.data.objects.new("Back_Light", backLight)
         constraint = ob.constraints.new(type='COPY_LOCATION')
         constraint.use_offset = True
@@ -121,7 +121,7 @@ class OBJECT_OT_add_light_template(Operator):
                 self.target = context.active_object
                 self.camera = context.scene.camera
 
-            add_lamps(self, context)
+            add_lights(self, context)
 
         except Exception as e:
             self.report({'WARNING'},

add_advanced_objects_menu/copy2.py
@@ -168,7 +168,7 @@ def copy_to_from(scene, to_obj, from_obj, copymode, axes, edgescale, scale):
 
     if copymode == 'E':
         # don't pass edgescalling to object types that cannot be scaled
-        if from_obj.type in ["CAMERA", "LAMP", "EMPTY", "ARMATURE", "SPEAKER", "META"]:
+        if from_obj.type in ["CAMERA", "LIGHT", "EMPTY", "ARMATURE", "SPEAKER", "META"]:
             edgescale = False
         edge_copy(scene, to_obj, from_obj, axes, edgescale, scale)
 

add_advanced_objects_menu/scene_objects_bi.py
@@ -47,7 +47,7 @@ class add_BI_scene(Operator):
             bpy.ops.view3d.viewnumpad(type='CAMERA')
 
             # add point lamp
-            bpy.ops.object.lamp_add(
+            bpy.ops.object.light_add(
                     type="POINT", location=(4.07625, 1.00545, 5.90386),
                     rotation=(0.650328, 0.055217, 1.866391)
                     )
@@ -59,7 +59,7 @@ class add_BI_scene(Operator):
             lamp1.use_sphere = True
 
             # add point lamp2
-            bpy.ops.object.lamp_add(
+            bpy.ops.object.light_add(
                     type="POINT", location=(-0.57101, -4.24586, 5.53674),
                     rotation=(1.571, 0, 0.785)
                     )

add_advanced_objects_menu/scene_objects_cycles.py
@@ -47,7 +47,7 @@ class add_cycles_scene(Operator):
             bpy.ops.view3d.viewnumpad(type='CAMERA')
 
             # add point lamp
-            bpy.ops.object.lamp_add(
+            bpy.ops.object.light_add(
                     type="POINT", location=(4.07625, 1.00545, 5.90386),
                     rotation=(0.650328, 0.055217, 1.866391)
                     )
@@ -59,7 +59,7 @@ class add_cycles_scene(Operator):
             lamp1.use_sphere = True
 
             # add point lamp2
-            bpy.ops.object.lamp_add(
+            bpy.ops.object.light_add(
                     type="POINT", location=(-0.57101, -4.24586, 5.53674),
                     rotation=(1.571, 0, 0.785)
                     )

add_advanced_objects_menu/trilighting.py
@@ -173,7 +173,7 @@ class TriLighting(Operator):
             backx = obj_position.x + self.distance * singleback_vector.x
             backy = obj_position.y + self.distance * singleback_vector.y
 
-            backData = bpy.data.lamps.new(name="TriLamp-Back", type=self.secondarytype)
+            backData = bpy.data.lights.new(name="TriLamp-Back", type=self.secondarytype)
             backData.energy = backEnergy
 
             backLamp = bpy.data.objects.new(name="TriLamp-Back", object_data=backData)
@@ -196,7 +196,7 @@ class TriLighting(Operator):
             rightx = obj_position.x + self.distance * singleright_vector.x
             righty = obj_position.y + self.distance * singleright_vector.y
 
-            rightData = bpy.data.lamps.new(name="TriLamp-Fill", type=self.secondarytype)
+            rightData = bpy.data.lights.new(name="TriLamp-Fill", type=self.secondarytype)
             rightData.energy = fillEnergy
             rightLamp = bpy.data.objects.new(name="TriLamp-Fill", object_data=rightData)
             scene.objects.link(rightLamp)
@@ -215,7 +215,7 @@ class TriLighting(Operator):
             leftx = obj_position.x + self.distance * singleleft_vector.x
             lefty = obj_position.y + self.distance * singleleft_vector.y
 
-            leftData = bpy.data.lamps.new(name="TriLamp-Key", type=self.primarytype)
+            leftData = bpy.data.lights.new(name="TriLamp-Key", type=self.primarytype)
             leftData.energy = keyEnergy
 
             leftLamp = bpy.data.objects.new(name="TriLamp-Key", object_data=leftData)

archimesh/__init__.py
@@ -53,7 +53,7 @@ if "bpy" in locals():
     importlib.reload(achm_kitchen_maker)
     importlib.reload(achm_shelves_maker)
     importlib.reload(achm_books_maker)
-    importlib.reload(achm_lamp_maker)
+    importlib.reload(achm_light_maker)
     importlib.reload(achm_curtain_maker)
     importlib.reload(achm_venetian_maker)
     importlib.reload(achm_main_panel)
@@ -66,7 +66,7 @@ else:
     from . import achm_venetian_maker
     from . import achm_door_maker
     from . import achm_kitchen_maker
-    from . import achm_lamp_maker
+    from . import achm_light_maker
     from . import achm_main_panel
     from . import achm_roof_maker
     from . import achm_room_maker
@@ -108,7 +108,7 @@ class AchmInfoMtMeshDecorationAdd(Menu):
     # noinspection PyUnusedLocal
     def draw(self, context):
         self.layout.operator("mesh.archimesh_books", text="Add Books")
-        self.layout.operator("mesh.archimesh_lamp", text="Add Lamp")
+        self.layout.operator("mesh.archimesh_light", text="Add Lamp")
         self.layout.operator("mesh.archimesh_roller", text="Add Roller curtains")
         self.layout.operator("mesh.archimesh_venetian", text="Add Venetian blind")
         self.layout.operator("mesh.archimesh_japan", text="Add Japanese curtains")
@@ -210,7 +210,7 @@ def register():
     bpy.utils.register_class(achm_kitchen_maker.AchmExportInventory)
     bpy.utils.register_class(achm_shelves_maker.AchmShelves)
     bpy.utils.register_class(achm_books_maker.AchmBooks)
-    bpy.utils.register_class(achm_lamp_maker.AchmLamp)
+    bpy.utils.register_class(achm_light_maker.AchmLamp)
     bpy.utils.register_class(achm_curtain_maker.AchmRoller)
     bpy.utils.register_class(achm_curtain_maker.AchmJapan)
     bpy.utils.register_class(achm_venetian_maker.AchmVenetian)
@@ -321,7 +321,7 @@ def unregister():
     bpy.utils.unregister_class(achm_kitchen_maker.AchmExportInventory)
     bpy.utils.unregister_class(achm_shelves_maker.AchmShelves)
     bpy.utils.unregister_class(achm_books_maker.AchmBooks)
-    bpy.utils.unregister_class(achm_lamp_maker.AchmLamp)
+    bpy.utils.unregister_class(achm_light_maker.AchmLamp)
     bpy.utils.unregister_class(achm_curtain_maker.AchmRoller)
     bpy.utils.unregister_class(achm_curtain_maker.AchmJapan)
     bpy.utils.unregister_class(achm_venetian_maker.AchmVenetian)

commit e2d8ea64b18ffdb932205e51585ad0f44ee4b737

Fix clamp variable name
This change fixes a bug introduced by:

https://github.com/sobotka/blender-addons/commit/6aa8e130eff59059886e203ff95221609f63b222

Which renamed many variables from "lamp" to "light" and
incorrectly renamed "clamp" to "clight" as part of a
search and replace.

io_scene_x3d/import_x3d.py
@@ -2816,9 +2816,9 @@ def appearance_LoadTexture(tex_node, ancestry, node):
 
     if bpyima:  # Loading can still fail
         repeat_s = tex_node.getFieldAsBool('repeatS', True, ancestry)
-        bpyima.use_clight_x = not repeat_s
+        bpyima.use_clamp_x = not repeat_s
         repeat_t = tex_node.getFieldAsBool('repeatT', True, ancestry)
-        bpyima.use_clight_y = not repeat_t
+        bpyima.use_clamp_y = not repeat_t
 
         # Update the desc-based cache
         if desc: